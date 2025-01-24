c5bbc2a7647e8d27997b7fd0b8a714f95f0e2044
Haris Kljajic <haris.kljajic@tink.com>
2025-01-24 09:16:24 +0100

    feat: support downloading and generating OSA

--- Example-Consumer-java/build.gradle.kts
+++ Example-Consumer-java/build.gradle.kts
@@ -1,21 +1,20 @@
 import io.ktor.http.*
 
 plugins {
-    id("java")
-    id( "au.com.dius.pact") version ("4.3.10")
+    java
+    id("au.com.dius.pact") version "4.3.10"
+    id("de.undercouch.download") version "5.6.0"
+    id("org.openapi.generator") version "6.6.0"
 }
 
 group = "org.example.consumer"
 version = "1.0-SNAPSHOT"
+val osaPlatformVersion = "2.0.0"
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
-}
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
@@ -27,7 +26,7 @@ dependencies {
     testImplementation("org.json:json:20220320")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("au.com.dius.pact.consumer:junit5:4.3.7")
+    testImplementation("au.com.dius.pact.consumer:junit5:4.3.10")
     testImplementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
@@ -49,4 +48,39 @@ tasks.getByName<Test>("test") {
     systemProperty("pact.consumer.version", project.version)
     systemProperty("pact.verifier.publishResults", "true")
     systemProperty("pact_do_not_track", "true")
+}
+
+tasks.register("downloadOSA") {
+    doLast {
+        val targetUrl = "https://api.github.com/repos/OWNER/REPO/contents/specifications/resources/.../$osaPlatformVersion/ping_$osaPlatformVersion.yaml"
+        val destinationFile = layout.buildDirectory.file("${layout.buildDirectory}/tmp/ping_$osaPlatformVersion.yaml").get().asFile
+        download.run {
+            src(targetUrl)
+            dest(destinationFile)
+
+            val ghToken = System.getenv("GITHUB_TOKEN")
+            if (ghToken.isNotEmpty()) {
+                header("Accept", "application/vnd.github.raw+json")
+                header("Authorization", "Bearer $ghToken")
+                header("X-GitHub-Api-Version", "2022-11-28")
+            }
+        }
+    }
+}
+defaultTasks("downloadOSA")
+
+openApiGenerate {
+    generatorName = "spring"
+    inputSpec = "${layout.buildDirectory.get()}/tmp/ping_$osaPlatformVersion.yaml"
+    outputDir = "${layout.buildDirectory.get()}/generated"
+    apiPackage = "com.platform.generated.api"
+    modelPackage = "com.platform.generated.model"
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("$${layout.buildDirectory.get()}/generated/src/main/java")
+        }
+    }
 }
